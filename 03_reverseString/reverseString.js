197bc0197187802dc1ede21093a1196f3f5dc82b
quinn <q.taylor@gmail.com>
2023-01-09 16:53:12 -0500

    completed exercise 03_revereString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    // reverse the string by slicing one character at a time and adding to reverse
+    // loop based on length of the string and use slice to get each character individually
+    let reverse = '';
+    for (let i = 0; i <= text.length; i++) {
+        reverse += text.slice(text.length - (i + 1), text.length - i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
