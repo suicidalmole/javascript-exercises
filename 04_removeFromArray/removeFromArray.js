d9b6abe76c3c9b96f368dc9b65257604295582da
quinn <q.taylor@gmail.com>
2023-01-09 18:08:06 -0500

    completed task 1 for removeFromArrayz

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+    array.splice(array.indexOf(item), 1);
+    return array;
 };
+
+// use indexOf() to find specific values in the array
+// use splice() with indexOf() to remove specific values
+// use includes () to test if item is present in the array
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
