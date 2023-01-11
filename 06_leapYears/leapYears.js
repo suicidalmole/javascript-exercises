8bd5ac1dffe7d94410c09679a0f1541dc25a4c99
quinn <q.taylor@gmail.com>
2023-01-11 18:44:41 -0500

    function works for years divisible by 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const result = (year % 4 == 0);
+    return result;
 };
+
+// leap years are divisible by 4
 
 // Do not edit below this line
 module.exports = leapYears;
