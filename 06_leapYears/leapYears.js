54de7aca62cf35a2c669760b1a7073426d6e49a5
quinn <q.taylor@gmail.com>
2023-01-11 18:54:52 -0500

    tests for leap year returns false if century year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,15 @@
 const leapYears = function(year) {
-    const result = (year % 4 == 0);
-    return result;
+    if (!(year % 100 == 0)) {               // test if century
+        const result = (year % 4 == 0);     // test if leap year
+        return result;
+    } else {
+        const result = false;
+        return result;
+    }
 };
 
 // leap years are divisible by 4
+// centuries are only leap years if they are divisble by 400
 
 // Do not edit below this line
 module.exports = leapYears;
