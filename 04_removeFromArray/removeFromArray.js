4eaef795cbe161efbb4dd19b47385bfd962315ed
quinn <q.taylor@gmail.com>
2023-01-11 14:29:58 -0500

    completed second test for removing multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,17 @@
-const removeFromArray = function(array, item) {
-    array.splice(array.indexOf(item), 1);
+const removeFromArray = function(array, ...items) {
+    for (const item of items) {
+        array.splice(array.indexOf(item), 1);
+    }
     return array;
 };
 
 // use indexOf() to find specific values in the array
 // use splice() with indexOf() to remove specific values
+
 // use includes () to test if item is present in the array
+
+// use the function rest parameter(...) for indefinite number of items to remove
+// iterate through the rest parameter array items and splice each value from the array
 
 
 
