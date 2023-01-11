a0a64f9ec76c41516aac2cfeddf5b24512109be9
quinn <q.taylor@gmail.com>
2023-01-11 15:18:31 -0500

    found start and end of range iterate through range and sum each number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(parameter_1, parameter_2) {
+    let sum = 0;
+    let start = Math.min(parameter_1, parameter_2);
+    const end = Math.max(parameter_1, parameter_2);
+    for (start; start <= end; start++) {
+        sum += start;
+    }
+    return sum;
 };
+
+// find the start and end of the range
+// iterate through each number including the beginning and end of the range
+// store sum in variable sum
 
 // Do not edit below this line
 module.exports = sumAll;
