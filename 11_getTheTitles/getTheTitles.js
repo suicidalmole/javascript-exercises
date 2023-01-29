5a49066d26076fb9bcce4b7ac3539d6a5449a406
quinn <q.taylor@gmail.com>
2023-01-29 01:03:24 -0500

    creates array of titles by iterating through array elements and accessing object properties

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titlesArray = [];
+    for (const element of array) {
+        const {title} = element;
+        titlesArray.push(title);
+    }
+    console.log({titlesArray});
+    return titlesArray;
 };
+
+//iterate through array
+//access 'title' property of each object in the array
+//add title from each object to new array
 
 // Do not edit below this line
 module.exports = getTheTitles;
