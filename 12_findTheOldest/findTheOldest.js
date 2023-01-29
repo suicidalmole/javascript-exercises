20ca97ee44c2892072fc51579275bc513a790860
quinn <q.taylor@gmail.com>
2023-01-29 16:09:27 -0500

    age sort moved to compareAges function age found using currentYear if no yearOfDeath

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,21 @@
 const findTheOldest = function(array) {
-    array.sort((a, b) => ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? 1 : -1))
-    return array[array.length - 1];
+    array.sort(compareAges);
+    return array[array.length - 1]; //return last object in array after sorting is complete
+};
+
+function compareAges(a, b) {
+    const currentYear = new Date().getFullYear();
+    return ('yearOfDeath' in a && 'yearOfDeath' in b ?         //check that yearOfDeath prop is present in both objects                 
+        ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? 1 : -1):  // return 1 if age of a > age of b
+            (!('yearOfDeath' in a) ?       //yearOfDeath prop not present in one or both objects so check object a
+                ((currentYear - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? 1 : -1) :  //not present in a, return 1 if a>b
+                    ((a.yearOfDeath - a.yearOfBirth) > (currentYear - b.yearOfBirth) ? 1 : -1)) //present in a, return 1 if a>b
+    );
 };
 
 // iterate through array of objects
 // calculate age from birth and death properties
+// if no death then get age to current year
 // sort by oldest
 // return last object in array
 
