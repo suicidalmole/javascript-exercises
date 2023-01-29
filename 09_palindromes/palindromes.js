9b3b55be08143b313bbf421723594ae929bb4bb4
quinn <q.taylor@gmail.com>
2023-01-28 19:21:12 -0500

    got strings put into arrays and succesfully reversing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,10 +2,11 @@ const palindromes = string  => {
     let cleanedString = string.replace((/\W/g), '');
     console.log({cleanedString});
     const stringArray = cleanedString.split('');
-    const reverseArray = stringArray;
+    console.log({stringArray});
+    let reverseArray = stringArray;
     reverseArray.reverse();
     console.log({reverseArray});
-    return (reverseArray.toString() === stringArray.toString() ? true : false);
+    
     
 };
 // Do not edit below this line
