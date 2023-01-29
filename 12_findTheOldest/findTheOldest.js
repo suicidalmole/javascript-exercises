f892d85d8f8980117e054faf90dfb032559702e5
quinn <q.taylor@gmail.com>
2023-01-29 01:42:40 -0500

    sort array by oldest and return last object in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((a, b) => ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? 1 : -1))
+    return array[array.length - 1];
 };
+
+// iterate through array of objects
+// calculate age from birth and death properties
+// sort by oldest
+// return last object in array
 
 // Do not edit below this line
 module.exports = findTheOldest;
