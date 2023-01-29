b7d90a4e37a4d74a019a86d2a7a18649f4c53ab3
quinn <q.taylor@gmail.com>
2023-01-29 00:38:50 -0500

    made array for fibonacci sequence loop pushing numbers in sequence to array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    const fibSequence = [1, 1];
+    if (member == 1 || member == 2) {
+        return 1;
+    } else {
+        for (let i = 3; i <= member; i++) {
+            fibSequence.push((fibSequence[i - 3] + fibSequence[i - 2]));
+            let lastItem = fibSequence[fibSequence.length - 1];
+            if (i == member) {
+                return (lastItem);
+            }
+        }
+    } 
+}
+//
 
-};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
