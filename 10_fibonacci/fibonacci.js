6c55fdcd585b9a85338e2c0f6213d9d5ea3cde51
quinn <q.taylor@gmail.com>
2023-01-29 00:50:48 -0500

    added tests for negative numbers Number conversion for strings passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,19 @@
 const fibonacci = function(member) {
     const fibSequence = [1, 1];
-    if (member == 1 || member == 2) {
+    const memberNum = Number(member);
+    if (memberNum == 1 || memberNum == 2) {
         return 1;
-    } else {
-        for (let i = 3; i <= member; i++) {
+    } else if (memberNum > 0 ) {
+        for (let i = 3; i <= memberNum; i++) {
             fibSequence.push((fibSequence[i - 3] + fibSequence[i - 2]));
             let lastItem = fibSequence[fibSequence.length - 1];
-            if (i == member) {
+            if (i == memberNum) {
                 return (lastItem);
             }
         }
-    } 
+    } else if (member <= 0 || (typeof member) !== 'number') {
+        return "OOPS";
+    }
 }
 //
 
